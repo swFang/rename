9b1a2f17c5db19e24db8016f0f0d4cb41bfe263f
swFang <33407178+swFang@users.noreply.github.com>
2019-11-23 21:56:39 -0800

    Sending back a hello world msg
    Sends back a hello world msg from /polls endpt.

--- django_test/mysite/mysite/urls.py
+++ django_test/mysite/mysite/urls.py
@@ -14,25 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
-from django.urls import path
-from django.views.generic import RedirectView
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import include, path
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
-
-# Use include() to add paths from the catalog application 
-urlpatterns += [
-    path('catalog/', include('catalog.urls')),
-]
-
-urlpatterns += [
-    path('', RedirectView.as_view(url='catalog/', permanent=True)),
-]
-
-# Use static() to add url mapping to serve static files during development (only)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
